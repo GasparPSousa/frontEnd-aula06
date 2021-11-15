215f968854296ed091510d26a12572d5138e4ba6
GasparPSousa <gasparufrj@gmail.com>
2021-11-15 16:47:10 -0300

    Inserindo videos específicos de Valorant

--- src/components/Galeria/index.js
+++ src/components/Galeria/index.js
@@ -8,9 +8,10 @@ const Galeria = () => {
     <section id="galeria">
       <h2>Vídeos</h2>
       <div className="grid">
-        <Video url="https://www.youtube.com/embed/WmopcSXDQa8" title="Bossa Nova Covers 2021 - Cool Music" />
-        <Video url='https://www.youtube.com/embed/F2IyttjjPfM' title='Reggae Covers Popular Songs 2021' />
-        <Video url='https://www.youtube.com/embed/QtXby3twMmI' title='Coldplay - Adventure Of A Lifetime (Official Video)' />        
+        <Video url='https://www.youtube.com/embed/_5cPRJ8sK_o' title="O ALTO NÍVEL DO RADIANTE É INSANO!! - VALORANT" />
+        <Video url='https://www.youtube.com/embed/8FXXZnvUUYY' title='VALORANT Last Chance Qualifier - Grande Final' />
+        <Video url='https://www.youtube.com/embed/qwfiFwfnGP8' title='9 Minutos De 1v5 Insanos no Valorant...'/>
+        <Video url='https://www.youtube.com/embed/_-qhH0qkFx4' title='12 MINUTOS de Sorte ou Habilidade?' />       
       </div>
     </section>
   )
